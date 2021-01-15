c0c943cdc111ffe743fde98eef7b51e727e827b2
William Greene <will@Wills-Mac-mini.attlocal.net>
2021-01-15 15:14:46 -0500

    changed json structure to reflect timestamps and funcs to conform

--- formatting.py
+++ formatting.py
@@ -1,5 +1,5 @@
 def format_fucks(data):
     results = ''
     for user in data:
-        results += f' **{user}** \n     {data[user]} fucks\n'
+        results += f' **{user}** \n     {data[user]["total"]} fucks\n'
     return results
--- jsonhelpers.py
+++ jsonhelpers.py
@@ -1,4 +1,5 @@
 import json as js
+from datetime import datetime as dt
 
 def get_sex():
     with open('sexboard.json') as f:
@@ -8,9 +9,9 @@ def get_sex():
 def add_sex(user):
     try:
         data = get_sex()
-        data[user] += 1
+        data[user]['total'] += 1
     except KeyError:
-        data[user] = 1
+        data[user]['total'] = 1
     with open('sexboard.json', 'w') as f:
         js.dump(data, f)
 
