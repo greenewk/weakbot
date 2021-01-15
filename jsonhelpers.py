784c0378260d7790ad0d94b57f9559ffd91aa30f
Will Greene <greenew.k@gmail.com>
2021-01-15 16:25:22 -0500

    changed JSON structure to include full time data months weeks years and functions, removed TRY EXCEPT and added dict defaults

--- jsonhelpers.py
+++ jsonhelpers.py
@@ -1,5 +1,5 @@
 import json as js
-from datetime import datetime as dt
+from datetime import date
 
 def get_sex():
     with open('sexboard.json') as f:
@@ -7,11 +7,35 @@ def get_sex():
         return data
 
 def add_sex(user):
-    try:
-        data = get_sex()
-        data[user]['total'] += 1
-    except KeyError:
-        data[user]['total'] = 1
+    today = str(date.today())
+    week = (str(date.today().isocalendar()[1]) + '-' + 
+            str(date.today().isocalendar()[0]))
+    month = (str(date.today().month) + '-' +
+            str(date.today().year))
+    year = (str(date.today().year))
+
+    data = get_sex()
+    data.setdefault(user, {})
+
+    data[user].setdefault('total', 0)
+    data[user]['total'] += 1
+
+    data[user].setdefault('days', {})
+    data[user]['days'].setdefault(today, 0)
+    data[user]['days'][today] += 1
+
+    data[user].setdefault('weeks', {})
+    data[user]['weeks'].setdefault(week, 0)
+    data[user]['weeks'][week] += 1
+
+    data[user].setdefault('months', {})
+    data[user]['months'].setdefault(month, 0)
+    data[user]['months'][month] += 1
+
+    data[user].setdefault('years', {})
+    data[user]['years'].setdefault(year, 0)
+    data[user]['years'][year] += 1
+
     with open('sexboard.json', 'w') as f:
         js.dump(data, f)
 
