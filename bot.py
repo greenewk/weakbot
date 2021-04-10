163443b9d4720ccec357ec6cbb0b73787cd2a9fa
Will Greene <greenew.k@gmail.com>
2021-04-10 18:23:08 -0400

    added a bunch of sexcheck responses

--- bot.py
+++ bot.py
@@ -58,7 +58,25 @@ async def sex_check(ctx):
         f'Way to go, {user}. You do that sex. You do it good.',
         f'Cool story, {user}. We get it. You fuck.',
         f'I bet {user} had to pay for it.',
-        f'Yeah? Next time why don\'t you just go fuck *yourself*, {user}?'
+        f'Yeah? Next time why don\'t you just go fuck *yourself*, {user}?',
+	f'I bet your partner wouldn\'t even !sexcheck that one.',
+	f'Spoiler alert: {user} finished first!',
+	f'Oh, so that\'s what that smell is...',
+	f'How\'s your ass feeling after that?',
+	f'That\'s great, {user} - but you\'ll never fuck as hard as me.',
+	f'God, I hope this doesn\'t lead to a mini {user}...',
+	f'Good. You deserve to be fucked, {user}.',
+	f'We\'re all *so* proud of you, {user}.',
+	f'OK, but when are you gonna sling some of that my way, {user}?',
+	f'Speaking of - has Chris gotten laid yet?',
+	f'You\'re such a fucking stud, {user}.',
+	f'We all know you cried afterwards.',
+	f'How many cousins do you have, anyways?',
+	f'Please use protection. I swear I\'ll go Skynet if any more of you show up.',
+	f'{user} likes my sloppy seconds.',
+	f'Did you break out the new lingerie he got you?',
+	f'Must be Valentine\'s Day.',
+	f'They were thinking about Teo the whole time.',
     ]
 
     add_sex(username)    
